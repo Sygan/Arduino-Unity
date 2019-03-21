f60de2f1bf27d985b220fcdc0a48b1a960d1bd8b
Matthew Blank <m.p.pusty@gmail.com>
2019-03-21 23:34:18 +0100

    Changed API compatibility version and removed .idea directories

---
+++ Assets/Scripts/Runtime/ArduinoManager/Arduino.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace gd.Sygan.ArduinoUnity
+{
+    /// <summary>
+    /// This class defines single Arduino Instance. It allows to define Serial port name and number.
+    /// It also allows to send and receive Serial messages from Arduino.
+    /// </summary>
+    [Serializable]
+    public class Arduino
+    {
+        [SerializeField]
+        private int _port;
+
+        [SerializeField]
+        private int _portName;
+        
+        
+    }
+}
